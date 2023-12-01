bd857a17059ffac67520deb09dc4952a24276472
TDWolff <torin.dean.wolff@gmail.com>
2023-12-01 10:15:32 -0800

    sure

---
+++ imggernator.py
@@ -0,0 +1,25 @@
+import requests
+from PIL import Image
+import io
+
+API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-1"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.content
+
+# Get user input as a prompt
+user_prompt = input("Enter prompt: ")
+
+# Replace spaces with underscores in the user's input
+filename = user_prompt.replace(" ", "_")
+
+# Query the model with the user's prompt
+image_bytes = query({"inputs": user_prompt})
+
+# Convert the image bytes to a PIL Image
+image = Image.open(io.BytesIO(image_bytes))
+
+# Save the image with a filename based on the user's input (spaces replaced with underscores)
+image.save(f"{filename}.png")
