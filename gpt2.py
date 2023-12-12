7444d83cb377f0801b221c3e7601928daac8dad4
TDWolff <torin.dean.wolff@gmail.com>
2023-12-12 10:03:43 -0800

    gpt2 work

--- gpt2.py
+++ gpt2.py
@@ -1,20 +1,2 @@
-import requests
-
-API_URL = "https://api-inference.huggingface.co/models/google/flan-t5-base"
-headers = {"Authorization": "Bearer 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"}
-
-def query(payload):
-    response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()
-
-# Get user input from the terminal
-user_input = input("Enter your question or input text: ")
-
-# Query the model with user input
-output = query({
-    "inputs": user_input,
-})
-
-# Display the entire model response
-print("Model Response:")
-print(output)
+from transformers import AutoModel
+model = AutoModel.from_pretrained("TheBloke/goliath-120b-GGUF")
